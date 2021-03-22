92d7b323807ee73a7c5e46013951ba0cb5a82e5a
Kevin Su <kevinsu@outlook.co.nz>
2021-03-22 17:45:50 +1100

    changes

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
       <div style = {{backgroundColor: "#fafafa", minHeight:"1000px"}}>
         <div class = "row" style = {{backgroundColor:"#FFEEE2", height:"1000px"}}>
-            <div class="wrapper" style = {{backgroundColor:"#FFEEE2", height:"1000px"}}>
+            <div class="wrapper" style = {{backgroundColor:"#FFEEE2", height:"1000px"}} id = "about">
                 <div class="three" style = {{backgroundColor:"white", padding:"40px", zIndex:"1"}}>
                    <h1 style = {{fontSize: "3.5em", fontFamily: "Playfair Display"}}>Hi I'm Kevin Su.</h1>
                    <br></br>
@@ -99,7 +99,7 @@ function App() {
         <div class = "row" style = {{backgroundColor:"#FFEEE2", minHeight:"100px"}}>
             <div class = "row70" style = {{backgroundColor:"#FFEEE2", textAlign:"center"}}>
                 <div class = "column">
-                    <h5 style = {{fontSize: "2em", fontFamily: "Playfair Display", marginTop: "10%", marginBottom:"20px", color:"#c85349"}}>Projects</h5>
+                    <h5 style = {{fontSize: "2em", fontFamily: "Playfair Display", marginTop: "10%", marginBottom:"20px", color:"#c85349"}} id = "projects">Projects</h5>
                     <hr style = {{marginBottom: "10px", width: "50%", backgroundColor: "#ff6666", height:"5px"}}></hr>
                 </div>
             </div>
@@ -163,98 +163,92 @@ function App() {
             </div>
         </div>
         <div class = "row" style = {{backgroundColor:"#FFEEE2", minHeight:"100px"}}>
-            <div class = "row80">
-
+            <div class = "row70" style = {{textAlign:"center", justifyContent: "center", alignItems:"center"}}>
+                <h3 style = {{fontFamily: "Playfair Display", fontSize: "40px", color:"#c85349"}} id = "exp"  >Experience</h3>
             </div>
-        </div>
-        <h3 style = {{color: "black", fontFamily: "Roboto", marginTop: "40px", fontSize: "40px"}} id = "exp"  >Experience</h3>
-        <h3 style = {{textAlign: "left", marginTop:"50px"}}>  </h3>
-        <div class = "row" style = {{alignItems:"center", justifyContent:"center", position:"absolute", marginLeft:"0px"}}>
-            <div class = "bluebox"></div>
-        </div>
-        <div class = "row70">
-            <div class = "row" style = {{marginBottom:"50px"}}></div>
-            <div class = "column" style = {{alignItems:"center"}}>
-                <Card style={{ width: '90%', height:"auto", textAlign:"left", minHeight:"525px"}}>
-                     <Card.Body>
-                       <div style = {{textAlign:"center", marginBottom:"30px"}}>
-                        <i class="fas fa-tasks fa-5x" style = {{color: "#ff6666"}}></i>
-                       </div>
-                       <Card.Title style = {{color: "black", fontFamily: "Roboto", fontSize:"25px"}}>Assistant Manager</Card.Title>
-                       <Card.Text >
-                         <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>Eden Foods Limited</h3>
-                         <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>2016 - 2018</h3>
-                         <hr></hr>
-                         Aided in stock management and control of the daily workflow within the store
-                         to maintain efficiency and order. Provided excellent customer service via professional and friendly communication.
-                         <hr></hr>
-                         <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>Location: Auckland, NZ</h3>
-                       </Card.Text>
-                     </Card.Body>
-                   </Card>
-             </div>
-             <div class = "column" style = {{alignItems:"center"}}>
-                 <Card style={{ width: '90%', height:"auto", textAlign:"left", minHeight:"525px"}}>
-                    <Card.Body>
-                      <div style = {{textAlign:"center", marginBottom:"30px"}}>
-                        <i class="fas fa-cog fa-5x" style = {{color: "#ff6666"}}></i>
-                      </div>
-                      <Card.Title style = {{color: "black", fontFamily: "Roboto", fontSize:"25px"}}>Unit Testing Intern</Card.Title>
-                      <Card.Text >
-                        <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>GPOS Limited</h3>
-                        <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>Dec 2018 - Feb 2019</h3>
-                        <hr></hr>
-                        Performed black box testing on a general point of sales software application using Postman and manually testing the application itself.
-                        <hr></hr>
-                        <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>Location: Auckland, NZ</h3>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
+            <hr style = {{marginBottom: "10px", width: "50%", backgroundColor: "#ff6666", height:"5px", marginBottom:"80px"}}></hr>
+            <div class = "row80" style = {{minHeight:"600px"}}>
+                <div class = "column" style = {{alignItems:"center", marginBottom:'20px'}}>
+                    <Card style={{ width: '90%', height:"auto", textAlign:"left"}}>
+                         <Card.Body>
+                           <div style = {{textAlign:"center", marginBottom:"30px"}}>
+                            <i class="fas fa-tasks fa-5x" style = {{color: "#ff6666"}}></i>
+                           </div>
+                           <Card.Title style = {{color: "black", fontSize:"25px"}}>Assistant Manager</Card.Title>
+                           <Card.Text >
+                             <h3 style = {{color: "black", fontSize:"15px"}}>Eden Foods Limited</h3>
+                             <h3 style = {{color: "black", fontSize:"15px"}}>2016 - 2018</h3>
+                             <hr></hr>
+                             Aided in stock management and control of the daily workflow within the store
+                             to maintain efficiency and order. Provided excellent customer service via professional and friendly communication.
+                             <hr></hr>
+                             <h3 style = {{color: "black", fontSize:"15px"}}>Location: Auckland, NZ</h3>
+                           </Card.Text>
+                         </Card.Body>
+                       </Card>
+                 </div>
+                 <div class = "column" style = {{alignItems:"center", marginBottom:'20px'}}>
+                     <Card style={{ width: '90%', height:"auto", textAlign:"left"}}>
+                        <Card.Body>
+                          <div style = {{textAlign:"center", marginBottom:"30px"}}>
+                            <i class="fas fa-cog fa-5x" style = {{color: "#ff6666"}}></i>
+                          </div>
+                          <Card.Title style = {{color: "black", fontSize:"25px"}}>Unit Testing Intern</Card.Title>
+                          <Card.Text >
+                            <h3 style = {{color: "black",fontSize:"15px"}}>GPOS Limited</h3>
+                            <h3 style = {{color: "black", fontSize:"15px"}}>Dec 2018 - Feb 2019</h3>
+                            <hr></hr>
+                            Performed black box testing on a general point of sales software application using Postman and manually testing the application itself.
+                            <hr></hr>
+                            <h3 style = {{color: "black", fontSize:"15px"}}>Location: Auckland, NZ</h3>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                  </div>
+                  <div class = "column" style = {{alignItems:"center", marginBottom:'20px'}}>
+                       <Card style={{ width: '90%', height:"auto", textAlign:"left"}}>
+                          <Card.Body>
+                            <div style = {{textAlign:"center", marginBottom:"30px"}}>
+                            <i class="fas fa-file-code fa-5x" style = {{color: "#ff6666"}}></i>
+                           </div>
+                            <Card.Title style = {{color: "black", fontSize:"25px"}}>Front End Intern</Card.Title>
+                            <Card.Text >
+                              <h3 style = {{color: "black", fontSize:"15px"}}>GPOS Limited</h3>
+                              <h3 style = {{color: "black", fontSize:"15px"}}>Dec 2019 - Feb 2020</h3>
+                              <hr></hr>
+                              Developed templates for a payments screen using React and Ant Design. Refactored the website's code from vanilla HTML into a React Application. Wrote
+                              extensive integration and unit tests for the created payments screen. Engaged with Agile Methodologies (weekly sprints). Improved
+                              presentation skills via presenting the product to the team.
+                              <hr></hr>
+                              <h3 style = {{color: "black", fontSize:"15px"}}>Location: Auckland, NZ</h3>
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                   </div>
               </div>
-              <div class = "column" style = {{alignItems:"center"}}>
-                   <Card style={{ width: '90%', height:"auto", textAlign:"left", minHeight:"525px"}}>
-                      <Card.Body>
-                        <div style = {{textAlign:"center", marginBottom:"30px"}}>
-                        <i class="fas fa-file-code fa-5x" style = {{color: "#ff6666"}}></i>
-                       </div>
-                        <Card.Title style = {{color: "black", fontFamily: "Roboto", fontSize:"25px"}}>Front End Intern</Card.Title>
-                        <Card.Text >
-                          <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>GPOS Limited</h3>
-                          <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>Dec 2019 - Feb 2020</h3>
-                          <hr></hr>
-                          Developed templates for a payments screen using React and Ant Design. Refactored the website's code from vanilla HTML into a React Application. Wrote
-                          extensive integration and unit tests for the created payments screen. Engaged with Agile Methodologies (weekly sprints). Improved
-                          presentation skills via presenting the product to the team.
-                          <hr></hr>
-                          <h3 style = {{color: "black", fontFamily: "Roboto", fontSize:"15px"}}>Location: Auckland, NZ</h3>
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-               </div>
         </div>
-        <div style = {{backgroundColor: "#0C2D48", fontFamily: "Roboto", minHeight:"100%", marginTop:"50px"}} id = "con">
-             <h3 style = {{backgroundColor: "#0C2D48", minHeight:"40px"}}></h3>
-             <div class = "row">
+        <div class = "row" style = {{backgroundColor: "#c85349"}} id = "con">
+             <div class = "row70">
                 <div class = "column">
-                    <h3 style = {{color: "#cedeef", fontFamily: "Roboto", fontSize: "40px"}}>Contact Me</h3>
+                    <h3 style = {{color: "white", fontSize: "40px", marginTop:"50px"}}>Contact Me</h3>
                     <h3 style = {{textAlign: "left", marginTop:"50px"}}>  </h3>
                 </div>
              </div>
-             <div class = "row">
-                <div class = "column" style = {{minHeight:"500px"}}>
-                     <h3 style = {{color: "white", fontFamily: "Roboto", fontSize:"30px"}}>Let's get in touch!</h3>
-                     <h3 style = {{color: "white", fontFamily: "Roboto", fontSize:"20px", marginTop:"20px"}}>Mobile: +64 22 584 0669</h3>
-                     <a href="https://www.linkedin.com/in/kevin-su-618163191/"><i class="fab fa-linkedin fa-2x" style = {{padding:"20px"}}></i></a>
-                             <a href="https://github.com/SuKaiwen"><i class="fab fa-github fa-2x" style = {{padding:"20px"}}></i></a>
-                             <a href="mailto:kevinsu@outlook.co.nz"><i class="fa fa-envelope fa-2x" style = {{padding:"20px"}}></i></a>
+             <div class = "row70">
+                <div class = "column" style = {{minHeight:"200px"}}>
+                     <h3 style = {{color: "white", fontSize:"20px"}}>Let's get in touch!</h3>
+                     <h3 style = {{color: "white", fontSize:"20px", marginTop:"20px"}}>Mobile: +64 22 584 0669</h3>
+                     <a href="https://www.linkedin.com/in/kevin-su-618163191/"><i class="fab fa-linkedin fa-2x" style = {{padding:"20px", color:"white"}}></i></a>
+                     <a href="https://github.com/SuKaiwen"><i class="fab fa-github fa-2x" style = {{padding:"20px", color:"white"}}></i></a>
+                     <a href="mailto:kevinsu@outlook.co.nz"><i class="fa fa-envelope fa-2x" style = {{padding:"20px", color:"white"}}></i></a>
                  </div>
-                <div class = "column" style = {{minHeight:"500px", alignItems:"center", textAlign:"left"}}>
-                    <img src={cat} alt="Logo" style = {{maxWidth:"250px", maxHeight:"250px", borderRadius: 300/ 2 }}/>
-                    <h3 style = {{color: "white", fontFamily: "Roboto", fontSize:"20px", marginTop:"20px"}}>Thank you for making it all the way to the end!</h3>
+                <div class = "column" style = {{minHeight:"350px", alignItems:"center", textAlign:"left"}}>
+                    <img src={cat} alt="Logo" style = {{maxWidth:"200px", maxHeight:"200px", borderRadius: 300/ 2 }}/>
+                    <h3 style = {{color: "white", fontSize:"20px", marginTop:"20px"}}>Thank you for making it all the way to the end!</h3>
                 </div>
              </div>
-         </div>
-      </div>
+            </div>
+        </div>
     </div>
   );
 }
